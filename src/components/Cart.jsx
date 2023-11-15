e92d224b4434fc304daecf0edda642f82e673eae
Muhammed Javis <muhammedjavis@gmail.com>
2023-11-15 17:30:19 +0530

    added Cart component which uses a new Context

---
+++ src/components/Cart.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import CartContext from '../store/CartContext';
+import { currencyFormatter } from '../util/formatter';
+import Button from './UI/Button';
+import UserProgressContext from '../store/UserProgressContext';
+import Modal from './UI/Modal';
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className='cart' open={userProgressCtx.progress === 'cart'}>
+      <h2>You Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
+      <p className='modal-actions'>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button>Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
